bee2d647993e2944f947930b2d275ed422bdab5c
chris284891 <chris284891@gmail.com>
2020-03-04 12:51:15 -0500

    added a signup page within login component and a console log for the username and password when signing up

--- src/app/common/auth/login/login.component.ts
+++ src/app/common/auth/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
     templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
     userName = '';
     password = '';
+    showSignup = false;
+    // showLogin = true;
 
     constructor(private router: Router, private authService: AuthService) {}
+
+    ngOnInit(): void {
+        console.log('comes on the screen');
+    }
+
+    ngOnDestroy(): void {
+        console.log('its destroyed')
+    }
+
+    toggleSignup(): void {
+        this.showSignup = !this.showSignup,
+        this.userName='',
+        this.password=''
+    }
+
+    toggleLogin (): void {
+        this.showSignup = !this.showSignup,
+        this.userName='',
+        this.password=''
+    }
 
     login(): void {
         this.authService.login(this.userName, this.password).subscribe(
@@ -24,4 +46,13 @@ export class LoginComponent {
             },
         );
     }
+
+    signup(): void {
+        const payload = {
+            email: this.userName,
+            password: this.password
+        }
+        console.log(payload);
+    }
+
 }
