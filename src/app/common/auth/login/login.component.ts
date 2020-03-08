3ed5f69f8b269bff6b94f239073a25af854f5e79
chris284891 <chris284891@gmail.com>
2020-03-08 19:09:27 -0400

    cut and pasted tavernsService files into login folder

--- src/app/common/auth/login/login.component.ts
+++ src/app/common/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { TavernsService } from 'src/taverns.service';
+import { TavernsService } from './taverns.service';
 
 @Component({
     templateUrl: './login.component.html',
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     password = '';
     showSignup = false;
 
-    constructor(private router: Router, private authService: AuthService) { }
+    constructor(private router: Router, private authService: AuthService, private tavernsService: TavernsService) { }
 
     ngOnInit(): void {
         console.log('comes on the screen');
