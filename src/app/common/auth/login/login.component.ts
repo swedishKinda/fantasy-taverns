6a3809258e53371887edb6d9ea8260ea52e901e6
Christopher Hedlund <CHedlund@milestraining.lab>
2020-03-20 12:20:24 -0400

    took out signup bits out of login component and template and made seperate signup component and template

--- src/app/common/auth/login/login.component.ts
+++ src/app/common/auth/login/login.component.ts
@@ -1,90 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
     templateUrl: './login.component.html',
 })
 
-export class LoginComponent implements OnInit, OnDestroy {
-    userName = '';
-    password = '';
-    adminTavernInput = '';
-    selected: any;
-    showSignup = false;
-    isAdmin = false;
-    Taverns = [{
-        Id:1,
-        Name:"John's Tavern"
-      },
-      {
-        Id:2,
-        Name:"Moe's Tavern"
-      },
-      {
-       Id:3,
-        Name:"Kate's Tavern"
-      }];
+export class LoginComponent {
+    loginForm = new FormGroup({
+        userName: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+    });
 
-    constructor(private router: Router, private authService: AuthService) { }
-
-    ngOnInit(): void {
-        console.log('comes on the screen');
-    }
-
-    ngOnDestroy(): void {
-        console.log('its destroyed')
-    }
-
-    toggleSignup(): void {
-        this.showSignup = !this.showSignup,
-            this.userName = '',
-            this.password = '',
-            this.isAdmin = false
-    }
+    constructor(private router: Router, private authService: AuthService) {}
 
     login(): void {
-        this.authService.login(this.userName, this.password).subscribe(
-            (response) => {
-                if (response.success) {
-                    console.log('successful login');
-                    this.router.navigateByUrl('/home');
-                }
-            },
-            (error) => {
-                console.log('username/password incorrect');
-            },
-        );
+        
+        console.log(this.loginForm);
+
+        this.authService
+            .login(this.loginForm.value.userName, this.loginForm.value.password)
+            .subscribe(
+                (response) => {
+                    if (response.success) {
+                        console.log('successful login');
+                        this.router.navigateByUrl('/taverns');
+                    }
+                },
+                (error) => {
+                    console.log('username/password incorrect');
+                },
+            );
     }
-
-    signup(): void {
-        const payload = {
-            UserName: this.userName,
-            Password: this.password,
-            tavern: {
-                TavernID: this.selected.Id,
-                // TavernName: this.isAdmin ? this.adminTavernInput : this.selected.Name
-            }
-        }
-        console.log(payload);
-
-        this.authService.create(payload).subscribe(
-            (user) => {
-                this.router.navigateByUrl('/login');
-                this.toggleSignup();
-                console.log('Successfuly Signed Up!');
-            },
-            (error) => {
-                console.log(error);
-            },
-        );
-    }
-
-    toggleAdmin(event) {
-        if (event.target.checked) {
-            this.isAdmin = true;
-            this.adminTavernInput = '';
-        }
-    }
-
 }
