8b58d7ce991827f911960c2d9b5bd754dd7cfbaa
Christopher Hedlund <CHedlund@milestraining.lab>
2020-03-28 15:25:32 -0400

    added some code to room component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { AuthService, IUser } from '../common/auth/auth.service';
+import { TavernService, IRoom } from '../tavern-services/tavern.service';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
 })
 export class RoomsComponent implements OnInit {
+
+  room: IRoom;
+  tavernName: string;
+  roomName: string;
+  dailyRate: number;
 
   constructor() { }
 
